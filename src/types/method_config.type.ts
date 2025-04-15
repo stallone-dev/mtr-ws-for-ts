719f86e1568750e928998de3346c78f12b2bac52
stallone-dev <stallone.developer@gmail.com>
2025-04-15 00:26:31 -0300

    chore(types): Reorganize files

---
+++ src/types/method_config.type.ts
@@ -0,0 +1,8 @@
+// Create by Stallone L. de Souza (@stallone-dev) - 2025 - License: MPL-2.0
+
+export type { WsMethodContext };
+
+interface WsMethodContext {
+    token: string;
+    baseUrl: string;
+}
--- src/types/ws_config.type.ts
+++ src/types/ws_config.type.ts
@@ -4,13 +4,13 @@ export { WsBaseURL, type WsClientConfig, type WsUserRole };
 
 type WsUserRole = "GERADOR" | "TRANSPORTADOR" | "DESTINADOR";
 
+interface WsClientConfig<R extends WsUserRole = WsUserRole> {
+    token: string;
+    baseUrl: WsBaseURL;
+    role: R;
+}
+
 enum WsBaseURL {
     SINIR = "https://admin.sinir.gov.br/apiws/rest",
     SIGOR = "https://mtrr.cetesb.sp.gov.br/apiws/rest",
-}
-
-interface WsClientConfig {
-    token: string;
-    baseUrl: WsBaseURL;
-    role: WsUserRole;
 }
