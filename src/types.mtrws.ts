22faaa2d1825786ba21a9aa35c7fa6fb80493e1b
stallone-dev <stallone.developer@gmail.com>
2025-06-21 23:21:25 -0300

    feat(poject): Add types central

---
+++ src/types.mtrws.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025, Stallone L. de Souza (@stallone-dev)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import type * as ConsultInterface from "~service/consult/consult.types.ts";
+import type * as DownloadInterface from "~service/download/download.types.ts";
+import type * as ReceiveInterface from "~service/receive/receive.types.ts";
+import type { WsClientConfig as WsClientInterface } from "~type/ws_config.type.ts";
+
+export type { ConsultInterface, DownloadInterface, ReceiveInterface, WsClientInterface };
