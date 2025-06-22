47672d0b6c1eecf6a8b8dfbf7c49c502259544de
stallone-dev <stallone.developer@gmail.com>
2025-06-21 23:17:35 -0300

    feat(utils): Add translate input base ws for ws url

---
+++ src/utils/translate_base_to_url.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025, Stallone L. de Souza (@stallone-dev)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { type WsBase, WsBaseURL } from "~type/ws_config.type.ts";
+
+export { WsBaseToBaseURL };
+
+function WsBaseToBaseURL(base: WsBase): WsBaseURL {
+    return BaseToUrlSchema[base];
+}
+
+const BaseToUrlSchema: Record<WsBase, WsBaseURL> = {
+    SIGOR: WsBaseURL.SIGOR,
+    SINIR: WsBaseURL.SIGOR,
+};
